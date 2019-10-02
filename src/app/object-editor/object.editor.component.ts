2dbe74342971002613f49a5c1341df17738cee2c
dafreels <dafreels@yahoo.com>
2019-10-02 14:29:02 -0400

    - Added save and cancel button to object editor

--- src/app/object-editor/object.editor.component.ts
+++ src/app/object-editor/object.editor.component.ts
@@ -15,15 +15,48 @@ export interface ObjectEditorDialogData {
   styleUrls: ['./object.editor.component.css']
 })
 export class ObjectEditorComponent {
+  formData;
+  formDefinition = [
+    '*',
+    {
+      type: 'submit',
+      title: 'Save'
+    },
+    {
+      type: 'button',
+      title: 'Cancel',
+      onClick: () => { this.cancelDialog() }
+    }
+  ];
+
   constructor(
     public dialogRef: MatDialogRef<ObjectEditorComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: ObjectEditorDialogData) {}
+    @Inject(MAT_DIALOG_DATA) public data: ObjectEditorDialogData) {
+    this.formData = {
+      schema: data.schema,
+      form: this.formDefinition,
+      data: data.userObject
+    };
+  }
 
   handleSchemaSelection(schemaName) {
     this.data.schema = JSON.parse(this.data.pkgObjs.find(p => p.id === schemaName).schema);
+    this.formData = {
+      schema: this.data.schema,
+      form: this.formDefinition,
+      data: this.data.userObject
+    };
   }
 
-  closeDialog(): void {
+  onSubmit(event): void {
+    this.data.userObject = event;
+    this.dialogRef.close({
+      userObject: event,
+      schemaName: this.data.schemaName
+    });
+  }
+
+  cancelDialog(): void {
     this.dialogRef.close();
   }
 }
