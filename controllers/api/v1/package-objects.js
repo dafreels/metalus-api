e91be84e39bd5262cbf664f4f3e0d08dac97cd9e
ebalog <ed.balogh@acxiom.com>
2019-11-05 14:10:48 -0500

    migrate package objects api routes to async/await

--- controllers/api/v1/package-objects.js
+++ controllers/api/v1/package-objects.js
@@ -6,17 +6,16 @@ const baseRoutes = new BaseRoutes('package-object', 'package-objects', PackageOb
 module.exports = function (router) {
     baseRoutes.buildBasicCrudRoutes(router);
 
-    router.patch('/:id/validate-object', (req, res) => {
-        baseRoutes.model.validateJson(req.params.id, req.body)
-            .then(result => {
-              if(result.isValid) {
+    router.patch('/:id/validate-object', async (req, res) => {
+        try {
+            const result = await baseRoutes.model.validateJson(req.params.id, req.body);
+            if(result.isValid) {
                 res.status(200).json(result);
-              } else {
+            } else {
                 res.status(422).json(result);
-              }
-            })
-            .catch(err => {
-                res.status(501).json({error: err});
-            });
+            }
+        } catch(err) {
+            res.status(501).json({error: err});
+        }
     });
 };
