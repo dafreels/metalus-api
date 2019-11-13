c72018d28a0c32e70503f59d583f795bee4d25fd
ebalog <ed.balogh@acxiom.com>
2019-11-13 11:47:17 -0500

    removed grunt test development options

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,32 +10,13 @@ module.exports = (grunt) => {
     'mocha_nyc:file'
   ]);
 
-  grunt.registerTask('test:file1', [
-    'clean:coverage',
-    'env:unit_test',
-    'mocha_nyc:file'
-  ]);
-
-  grunt.registerTask('test:mongo1', [
-    'clean:coverage',
-    'env:unit_test',
-    'mocha_nyc:mongo'
-  ]);
-
   grunt.registerTask('test:mongo', [
     'clean:coverage',
     'env:unit_test',
     'mocha_nyc:mongo'
   ]);
 
-
   grunt.registerTask('test:api', [
-    'clean:coverage',
-    'env:unit_test',
-    'mocha_nyc:all_tests'
-  ]);
-
-  grunt.registerTask('test:api1', [
     'clean:coverage',
     'env:unit_test',
     'mocha_nyc:all_tests'
