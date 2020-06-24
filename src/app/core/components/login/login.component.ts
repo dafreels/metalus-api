7d8afc99e7fb32c5f5cd74d3ac73be80071834a6
skonat <Suresh.Konatam@acxiom.com>
2020-06-24 13:13:39 -0500

    One last commit about warning message

--- src/app/core/components/login/login.component.ts
+++ src/app/core/components/login/login.component.ts
@@ -12,6 +12,7 @@ export class LoginComponent implements OnInit {
   username: String;
   userPassword: String;
   loginError: Boolean = false;
+  hasLoggedIn: boolean;
 
   constructor(private authService: AuthService, private router: Router) {}
 
