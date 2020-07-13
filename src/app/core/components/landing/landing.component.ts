bdb00d9e7f6d38fc481db47e8d9b79c55e57d0b4
skonat <Suresh.Konatam@acxiom.com>
2020-07-13 12:38:06 -0500

    Fixed bug in Drop Down project change and removed Navigation bar subscription multiple calls

--- src/app/core/components/landing/landing.component.ts
+++ src/app/core/components/landing/landing.component.ts
@@ -1,45 +1,29 @@
 import {PipelinesService} from '../../../pipelines/services/pipelines.service';
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {StepsService} from '../../../steps/steps.service';
 import {ApplicationsService} from '../../../applications/applications.service';
 import {PackageObjectsService} from '../../package-objects/package-objects.service';
-import {AuthService} from "../../../shared/services/auth.service";
-import {User} from "../../../shared/models/users.models";
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit, OnDestroy {
+export class LandingComponent implements OnInit {
   applicationCount = 0;
   pipelineCount = 0;
   packageObjectCount = 0;
   stepCount = 0;
-  user: User;
-  userSubscription;
 
   constructor(
     private applicationService: ApplicationsService,
     private packageObjectsService: PackageObjectsService,
     private pipelinesService: PipelinesService,
     private stepsService: StepsService,
-    private authService: AuthService) {
-    this.user = this.authService.getUserInfo();
-    this.userSubscription = this.authService.userItemSelection.subscribe(data => {
-      this.user = data;
-      this.loadCounts();
-    });
-  }
+    ) {}
 
   ngOnInit(): void {
     this.loadCounts();
-  }
-
-  ngOnDestroy(): void {
-    if (this.userSubscription) {
-      this.userSubscription.unsubscribe();
-    }
   }
 
   private loadCounts() {
--- src/app/core/components/nav-toolbar/nav-toolbar.component.ts
+++ src/app/core/components/nav-toolbar/nav-toolbar.component.ts
@@ -1,13 +1,26 @@
-import {Component, Input} from "@angular/core";
-import {User} from "../../../shared/models/users.models";
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+
+interface IRouterData {
+  page: string;
+  title: string;
+}
 
 @Component({
   selector: 'nav-toolbar',
   templateUrl: './nav-toolbar.component.html',
-  styleUrls: ['./nav-toolbar.component.scss']
+  styleUrls: ['./nav-toolbar.component.scss'],
 })
-export class NavToolbarComponent {
-  @Input() user: User;
+export class NavToolbarComponent implements OnInit {
   @Input() page: string;
   @Input() title: string;
+  routerData$: Observable<IRouterData>;
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  ngOnInit(){
+    this.activatedRoute.data.subscribe(data => {
+      this.title=data.title;
+      this.page = data.page;
+    });
+  }
 }
