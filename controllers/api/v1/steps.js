2cf0ee3863d3b76d9811cbf4bd22cc75b985b851
dafreels <dafreels@yahoo.com>
2020-12-29 14:30:40 -0500

    #102 Added two end points for working with step templates.

--- controllers/api/v1/steps.js
+++ controllers/api/v1/steps.js
@@ -1,9 +1,46 @@
 const StepsModel = require('../../../models/steps.model');
 const BaseRoutes = require('../../../lib/base.routes');
+const ValidationError = require('./ValidationError');
 
 module.exports = function (router) {
   const baseRoutes = new BaseRoutes('step', 'steps', StepsModel);
   baseRoutes.buildBasicCrudRoutes(router);
 
-  // custom routes go here
+  // custom routes
+  router.get('/:id/template', getTemplate);
+  router.put('/:id/template', updateTemplate);
 };
+
+async function getTemplate(req, res) {
+  try {
+    const stepsModel = new StepsModel();
+    const user = await this.getUser(req);
+    const record = await stepsModel.getTemplate(req.params.id, user);
+    if (record) {
+      const returnObj = {};
+      returnObj['stepTemplate'] = record;
+      res.json(returnObj);
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (err) {
+    res.status(501).json({error: err});
+  }
+}
+
+async function updateTemplate(req, res, next) {
+  try {
+    const stepsModel = new StepsModel();
+    const user = await this.getUser(req);
+    const record = await stepsModel.updateTemplate(req.params.id, req.body, user);
+    const returnObj = {};
+    returnObj['stepTemplate'] = record;
+    res.json(returnObj);
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      res.status(422).json({errors: err.getValidationErrors(), body: req.body});
+    } else {
+      next(err);
+    }
+  }
+}
--- models/steps.model.js
+++ models/steps.model.js
@@ -3,10 +3,55 @@ const BaseModel = require('../lib/base.model');
 
 
 class StepsModel extends BaseModel {
-    constructor() {
-        super('steps', schema);
+  constructor() {
+    super('steps', schema);
+  }
+
+  // custom model logic goes here
+  async getTemplate(id, user) {
+    const model = this.getStorageModel('stepTemplates');
+    const key = {
+      id,
+    };
+    if (user) {
+      key.project = {
+        userId: user.id,
+        projectId: user.defaultProjectId
+      };
     }
-    // custom model logic goes here
+    const records = await model.find(key);
+    if (records.length > 1) {
+      throw new Error('more than one record found for this key!');
+    } else if (records.length === 1) {
+      return records[0];
+    } else {
+      return null;
+    }
+  }
+
+  async updateTemplate(id, record, user) {
+    const model = this.getStorageModel('stepTemplates');
+    const key = {
+      id: record.id
+    };
+    let project;
+    if (user) {
+      project = {
+        userId: user.id,
+        projectId: user.defaultProjectId
+      };
+      record.project = project;
+      key.project = project;
+    }
+    // const validation = this.validator(record);
+    // if (!validation) {
+    //   throw new ValidationError('', this.validator.errors);
+    // } else if (id !== record.id) {
+    //   throw new ValidationError('', `update failed: id from object(${record.id}) does not match id from url(${id})`);
+    // } else {
+      return await model.updateRecord(key, record);
+    // }
+  }
 }
 
 module.exports = StepsModel;
