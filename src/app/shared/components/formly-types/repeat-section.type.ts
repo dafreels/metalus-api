da686ec8b54d76856e35df38c6627eb2b92fc701
dafreels <dafreels@yahoo.com>
2021-02-18 13:19:52 -0500

    - Increased the size of the project templates modal - Added a description attribute to the repeat form type - Add initial documentation for execution templates

--- src/app/shared/components/formly-types/repeat-section.type.ts
+++ src/app/shared/components/formly-types/repeat-section.type.ts
@@ -4,6 +4,7 @@ import { FieldArrayType } from '@ngx-formly/core';
 @Component({
   selector: 'formly-repeat-section',
   template: `
+    <h4>{{ to.description || '' }}</h4>
     <div *ngFor="let field of field.fieldGroup; let i = index;" class="section">
         <formly-field class="col" [field]="field"></formly-field>
         <button mat-icon-button color="warn" class="remove" type="button" (click)="remove(i)"><mat-icon>delete</mat-icon></button>
