bc145a8539c69f3bc834a9a61cf8c1709cc6f242
Suresh Konatam <suresh.konatam@acxiom.com>
2021-04-27 12:49:27 -0500

    Removed commented code

--- src/app/shared/components/schema-editor/schema-editor.component.ts
+++ src/app/shared/components/schema-editor/schema-editor.component.ts
@@ -7,31 +7,11 @@ import { Schema } from "../../models/schema.model";
 })
 export class SchemaEditorComponent implements OnInit{
 
-  // private _schemaObject:Schema
-  // @Input() set schemaObject(schemaObj){
-  //   console.log('schemaObj',schemaObj);
-  //   this._schemaObject = schemaObj;
-  //   this.change.emit(this._schemaObject);
-  // }
-  // get schemaObject() {
-  //   return this._schemaObject;
-  // }
   @Input('schemaObject') schemaObject: Schema;
   @Input() canSubmit:boolean = true;
-  // schemaObject;
   @Output() schemaChanged = new EventEmitter();
   ngOnInit(): void {
-    // this.schemaObject = new Proxy(this.schemaObjectInput, {
-    //   set: (target:any, key:string, value) => {
-    //     console.log(`${key} set to ${value}`);
-    //     this.change.emit(target);
-    //     target[key] = value;
-    //     return true;
-    //   }
-    // });
-    // setInterval(()=>{
-    //   console.log(this.schemaObject);
-    // }, 1500)
+    
   }
   addAttribute() {
     this.schemaObject.attributes.push({
